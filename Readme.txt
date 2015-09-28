fc9ccb018174f3a0de91c82877f4bb719fd70f25
Yuen Boon Yee <yuen_boon_yee@sereneit.com>
2015-09-29 07:59:21 +0800

    Added UI test.

--- Readme.txt
+++ Readme.txt
@@ -3,8 +3,11 @@ XCode: 7.0
 
 This project demonstrates how to set up automated UI Testing in Xcode.
 This method does not use UI Automation, which is a Javascript-based framework.
+Instead, it uses the XCTest and Accessibility frameworks to run and validate the tests.
 
 Method:
+Create a new Target. Choose iOS->Test->iOS UI Testing Bundle.
+	A new group and XCTestCase sub-class is created with an empty test example method.
 
 
 References:
---
+++ UITestingDemoUITests/UITestingDemoUITests.m
@@ -0,0 +1,62 @@
+//
+//  UITestingDemoUITests.m
+//  UITestingDemoUITests
+//
+//  Created by Serene IT on 29/09/2015.
+//  Copyright © 2015 Serene IT. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UITestingDemoUITests : XCTestCase
+
+@end
+
+@implementation UITestingDemoUITests
+
+- (void)setUp {
+    [super setUp];
+    
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    // In UI tests it is usually best to stop immediately when a failure occurs.
+    self.continueAfterFailure = NO;
+    // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+    [[[XCUIApplication alloc] init] launch];
+    
+    // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // Use recording to get started writing UI tests.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+/*!
+ @brief  Tests the creation of a new record, and deleting of that record.
+ @discussion Most of the code was inserted by the UI recording tool.
+ */
+-(void)testAddAndRemoveRecord{
+    
+    XCUIApplication *app = [[XCUIApplication alloc] init];
+    XCUIElement *masterNavigationBar = app.navigationBars[@"Master"];
+    [masterNavigationBar.buttons[@"Add"] tap];
+    [masterNavigationBar.buttons[@"Edit"] tap];
+    
+    XCUIElementQuery *tablesQuery = app.tables;
+    //The following code was added by the recording tool, but is label dependent and will not match any subsequently created records, rendering the test un-repeatable.
+//    [tablesQuery.buttons[@"Delete 2015-09-28 23:38:49 +0000"] tap];
+
+    //We'll use the following element selector instead, which will always tap on the first row's delete button.
+    [[tablesQuery.buttons elementBoundByIndex:0] tap];
+
+    [tablesQuery.buttons[@"Delete"] tap];
+    [masterNavigationBar.buttons[@"Done"] tap];
+    
+}
+@end
